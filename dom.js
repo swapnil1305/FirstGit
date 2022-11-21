05540932143abaf51a75e3d287616b76a65e625e
swapnil1305 <swapnil.satpute132@gmail.com>
2022-11-21 20:22:27 +0530

    push bytagname

--- dom.js
+++ dom.js
@@ -7,3 +7,13 @@ items[1].style.fontWeight='bold';
 items[2].style.fontWeight='bold';
 items[3].style.fontWeight='bold';
 items[2].style.backgroundColor='green';
+
+var items=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent="Hello 2";
+li[1].style.fontWeight="bold";
+li[1].style.backgroundColor="yellow";
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
+}
