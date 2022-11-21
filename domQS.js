1b2dccbff6d58f2b9ba481a06ed136d75622e994
swapnil1305 <swapnil.satpute132@gmail.com>
2022-11-21 20:48:08 +0530

    querySelector

---
+++ domQS.js
@@ -0,0 +1,7 @@
+var secondItem=document.querySelector
+('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem=document.querySelector
+('.list-group-item:nth-child(3)');
+thirdItem.style.color='white';
