3f730f7a040c2332adefc812a6f9417807c615dc
swapnil1305 <swapnil.satpute132@gmail.com>
2022-11-21 21:08:07 +0530

    QuerySelectorAll

---
+++ QuerySelectorAll.js
@@ -0,0 +1,8 @@
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
+
+var secondItem=document.querySelector
+('.list-group-item:nth-child(2)');
+secondItem.style.color='green';
